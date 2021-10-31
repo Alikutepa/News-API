f7c9af5ab0e39534585ef43fdec24dde1ea27a2a
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-10-31 07:46:56 +0300

    add base url

--- app/config.py
+++ app/config.py
@@ -25,3 +25,9 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?&apiKey='    
