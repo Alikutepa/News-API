b3e8b05698e6f9b98e2bd532a4d404336449d02b
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-10-31 11:21:22 +0300

    add test

--- articles_test.py
+++ articles_test.py
@@ -11,8 +11,7 @@ class ArticleTest(unittest.TestCase):
         '''
         Setup method that will run before every test
         '''
-        self.new_article = Article('bbc','bbc','silah koskei','Trump','Trump takes on swearing in of Kenya oposition leader',
-                                    'www.bbc.com/kenya','www.bbc.com/kenya/image.jpg',"28-01-2018T10.00.00EAT")
+        self.new_article = Article('bbc','bbc','silah koskei','Trump','Trump takes on swearing in of Kenya oposition leader','www.bbc.com/kenya','www.bbc.com/kenya/image.jpg',"28-01-2018T10.00.00EAT")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article, Article))
